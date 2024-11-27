97368cd9e153c978dc423569b69323d846e00ac2
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2024-11-27 23:47:20 +0300

    Sort products by category

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
+import collections
+import pprint  # TODO: delete
 
 from word_year_endings import get_correct_year_word
 
@@ -18,9 +20,18 @@ years_since_foundation = current_year.year - foundation_year.year
 
 year_word = get_correct_year_word(years_since_foundation)
 
-excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Лист1')
+excel_data_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1', keep_default_na=False)
 products = excel_data_df.to_dict(orient='records')
-print(products)
+pprint.pprint(products)  # TODO: delete!
+
+# TODO: delete!
+products_by_category = collections.defaultdict(list)
+for product in products:
+    products_by_category[product['Категория']].append(product)
+
+pprint.pprint(products_by_category)
+# TODO: delete!
+
 rendered_page = template.render(
     years_since_foundation=years_since_foundation,
     year_word=year_word,
