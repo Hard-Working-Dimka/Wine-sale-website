b5b990deae21db75d178212ac3527b7f28624d4b
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2024-11-30 00:25:40 +0300

    Use if_main structure and clear code

--- main.py
+++ main.py
@@ -3,37 +3,46 @@ import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
 import collections
+import os
+from dotenv import load_dotenv
 
 from word_year_endings import get_correct_year_word
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+FOUNDATION_YEAR = 1920
 
-template = env.get_template('template.html')
 
-foundation_year = datetime.datetime(year=1920, month=1, day=1, hour=0)
-current_year = datetime.datetime.today()
-years_since_foundation = current_year.year - foundation_year.year
+def main():
+    load_dotenv()
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
 
-year_word = get_correct_year_word(years_since_foundation)
+    excel_data_df = pandas.read_excel(os.getenv('EXEL_FILE_NAME'), sheet_name=os.getenv('EXEL_LIST_NAME'),
+                                      keep_default_na=False)
+    products = excel_data_df.to_dict(orient='records')
 
-excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', keep_default_na=False)
-products = excel_data_df.to_dict(orient='records')
+    current_year = datetime.datetime.today()
+    years_since_foundation = current_year.year - FOUNDATION_YEAR
+    year_word = get_correct_year_word(years_since_foundation)
 
-products_by_category = collections.defaultdict(list)
-for product in products:
-    products_by_category[product['Категория']].append(product)
+    products_by_category = collections.defaultdict(list)
+    for product in products:
+        products_by_category[product['Категория']].append(product)
 
-rendered_page = template.render(
-    years_since_foundation=years_since_foundation,
-    year_word=year_word,
-    products_by_category=products_by_category
-)
+    rendered_page = template.render(
+        years_since_foundation=years_since_foundation,
+        year_word=year_word,
+        products_by_category=products_by_category
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
